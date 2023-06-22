4ad99d256cac7bb0a7074b5146eb04f6e3860283
JubaStriker <jubastriker@gmail.com>
2023-06-22 10:23:26 +0600

    Push notification function added

--- index.js
+++ index.js
@@ -8,29 +8,50 @@ const xumm = new XummSdk(api_key, api_secret);
 
 const main = async () => {
 
+    let result;
+
     const request = {
-        // txjson: {
-        //     Account: "rNzHuiEJetmercrQ33UZi5BLA1pNo2cykx",
-        //     NFTokenSellOffer: "34A29A872AF29A4564E98F87F2019EAE91AECCB4F5D373D40C5316B55C01EE63",
-        //     TransactionType: "NFTokenMint",
-        //     TransferFee: 1,
-        //     NFTokenTaxon: 0,
-        //     Flags: 8,
-        //     URI: "697066733A2F2F62616679626569676479727A74357366703775646D37687537367568377932366E6634646675796C71616266336F636C67747179353566627A6469",
-        // }
         txjson: {
-            TransactionType: "NFTokenCreateOffer",
             Account: "rNzHuiEJetmercrQ33UZi5BLA1pNo2cykx",
-            NFTokenID: "00080000996F7636107365C8BE51C439F371C1C4D31C552F2DCBAB9D00000002",
-            Amount: "0",
-            Flags: 1,
-            Destination: "rNPPJe3wEoHxxfmxSRgcuVe8Hu2AFhRzfe"
-        }
+            NFTokenSellOffer: "34A29A872AF29A4564E98F87F2019EAE91AECCB4F5D373D40C5316B55C01EE63",
+            TransactionType: "NFTokenMint",
+            TransferFee: 1,
+            NFTokenTaxon: 0,
+            Flags: 8,
+            URI: "697066733A2F2F62616679626569676479727A74357366703775646D37687537367568377932366E6634646675796C71616266336F636C67747179353566627A6469",
+        },
+        // user_token: result.application.issued_user_token,
+        // txjson: {
+        //     TransactionType: "NFTokenCreateOffer",
+        //     Account: "rNzHuiEJetmercrQ33UZi5BLA1pNo2cykx",
+        //     NFTokenID: "00080000996F7636107365C8BE51C439F371C1C4D31C552F2DCBAB9D00000002",
+        //     Amount: "0",
+        //     Flags: 1,
+        //     Destination: "rNPPJe3wEoHxxfmxSRgcuVe8Hu2AFhRzfe"
+        // }
 
     }
 
-    const payload = await xumm.payload.create(request, true);
-    console.log(payload)
+    const subscription = await xumm.payload.createAndSubscribe(request, event => {
+        console.log("New payload event", event.data);
+
+        if (Object.keys(event.data).indexOf('signed') > -1) {
+            return event.data;
+        }
+    });
+
+
+    const resolvedData = await subscription.resolved;
+    if (resolvedData.signed == false) {
+        console.log("The sign request was rejected")
+    }
+    else {
+        console.log("The sign request was signed")
+        result = await xumm.payload.get(resolvedData.payload_uuidv4)
+        console.log("User token", result.application.issued_user_token)
+    }
+
+
 };
 
 
